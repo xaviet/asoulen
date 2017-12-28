057e63b87dfb28f19599dbe3efe65ee3cb160c0c
jin xu <pioevh@163.com>
2017-12-28 22:33:19 +0800

    tcp

--- asoulen/tcpcli/client.py
+++ asoulen/tcpcli/client.py
@@ -5,8 +5,29 @@
 '''
 
 
+import socket
+
+
+class client:
+
+  def __init__(self, **kwargs):
+    self.host = '127.0.0.1'
+    self.port = 60013
+    return super().__init__(**kwargs)
+
+  def run(self):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+      self.sock.connect((self.host, self.port))
+      self.sock.sendall(bytes('abc123ABC1456' + '\n', 'utf-8'))
+      self.received = str(self.sock.recv(1024), 'utf-8')
+    finally:
+      self.sock.close()
+
+
 def main():
-  pass
+  s=client()
+  s.run()
 
 
 if(__name__ == '__main__'):
--- asoulen/tcpcli/server.py
+++ asoulen/tcpcli/server.py
@@ -5,8 +5,33 @@
 '''
 
 
+import socketserver
+
+
+class handler(socketserver.BaseRequestHandler):
+ 
+    def handle(self):
+        self.data = self.request.recv(2048).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+
+class server:
+
+  def __init__(self, **kwargs):
+    self.host = '0.0.0.0'
+    self.port = 60013
+    return super().__init__(**kwargs)
+
+  def run(self):
+    self.sock = socketserver.TCPServer((self.host, self.port), handler)
+    self.sock.serve_forever()
+
+
 def main():
-  pass
+  s=server()
+  s.run()
 
 
 if(__name__ == '__main__'):
